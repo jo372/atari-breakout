9e51aed0efc4d239d4814bd436249afc154f6173
jo372 <J.O'Hara2@uni.brighton.ac.uk>
2020-02-29 11:24:44 +0000

    created a BrickObj which extends GameObj

---
+++ BrickObj.java
@@ -0,0 +1,7 @@
+import javafx.scene.paint.Color;
+
+public class BrickObj extends GameObj {
+    public BrickObj(int x, int y, int width, int height, Color color ) {
+        super(x, y, width, height, color);
+    }
+}

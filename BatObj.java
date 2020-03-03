c41fe072491c56acad6ae14250774ed6a0ae0b31
Jordan O'Hara (student) <jo372@uni.brighton.ac.uk>
2020-03-03 11:46:58 +0000

    created a seperate BatObj which extends the GameObj class.

---
+++ BatObj.java
@@ -0,0 +1,9 @@
+import javafx.scene.paint.Color;
+
+public class BatObj extends GameObj
+{
+    public BatObj(int x, int y, int width, int height, Color color)
+    {
+       super(x, y, width, height, color);
+    }
+}

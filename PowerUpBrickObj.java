23f6509975635578f57681951fcf438e7d9cdc54
Jordan O'Hara (student) <jo372@uni.brighton.ac.uk>
2020-03-10 15:31:40 +0000

    compiled the java documentation and removed the powerupbrickobj which is not being used.

--- PowerUpBrickObj.java
+++
@@ -1,9 +0,0 @@
-import javafx.scene.paint.Color;
-
-public class PowerUpBrickObj extends BrickObj
-{
-    public PowerUpBrickObj(int x, int y, int width, int height, Color color)
-    {
-        super(x, y, width, height, color);
-    }
-}

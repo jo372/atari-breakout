15649ce018ebd411e0a731d6334cc84b5ee6c9b3
Jordan O'Hara (student) <jo372@uni.brighton.ac.uk>
2020-03-11 10:41:06 +0000

    created debuff and powerup objects

---
+++ DebuffObj.java
@@ -0,0 +1,9 @@
+import javafx.scene.paint.Color;
+
+public class DebuffObj extends GameObj
+{
+    public DebuffObj(int x, int y, int width, int height, Color color)
+    {
+        super(x, y, width, height, color);
+    }
+}
---
+++ PowerUpObj.java
@@ -0,0 +1,12 @@
+import javafx.scene.paint.Color;
+
+public class PowerUpObj extends GameObj
+{
+    // what do I want the powerups to be able to do? 
+    // change speed
+    // 
+    public PowerUpObj(int x, int y, int width, int height, Color color)
+    {
+        super(x, y, width, height, color);
+    }
+}

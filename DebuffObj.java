d770164a293b919e20c06f648d623ff4f44cf124
unknown <J.O'Hara2@uni.brighton.ac.uk>
2020-05-01 07:21:30 +0100

    removing bluej created files

--- DebuffObj.java
+++
@@ -1,9 +0,0 @@
-import javafx.scene.paint.Color;
-
-public class DebuffObj extends BrickObj
-{
-    public DebuffObj(int x, int y, int width, int height, Color color)
-    {
-        super(x, y, width, height, color);
-    }
-}
